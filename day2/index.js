e478f0739d7995a5094565bf0717f95c3a10fe47
souravrajbhar1234567 <saurabhrajbhar223225@gmail.com>
2026-02-04 10:10:59 +0530

    fs module

--- day2/index.js
+++ day2/index.js
@@ -1,25 +1,41 @@
+f1();
+function f1(){
+    console.log("inside the function");  
+}
+f1();
 import fs from 'fs';
+const writeFileSync = (path, data) => {
+    try {
+        fs.writeFileSync(path, data);
+        console.log("File Has been created successfully");
+    } catch (error) {
+        console.log("Some Error occured...");
+    }
+}
+const readFileSync = (path) => {
+    try {
+        const data = fs.readFileSync(path,'utf-8');
+        console.log(data);
+    } catch (error) {
+        console.log("Some Error occured...");
+    }
+}
+const appendFileSync = (path, data) => {
+    try {
+        fs.appendFileSync(path,data);
+        console.log("File Has been appended successfully");
+    } catch (error) {
+        console.log("Some Error occured...");
+    }
+}
+console.log("Before write");
+writeFileSync("./example.txt", "This is the data written through sync func:'writeFileSync'")
+console.log("after write");
 
-console.log(fs);
+console.log("Before read");
+readFileSync("./example.txt");
+console.log("after read");
 
-
-const writeFileSyncFn = (path, data) => {
-  try {
-    fs.writeFileSync(path, data, 'utf-8');
-    console.log('File written successfully');
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const readFileSyncFn = (path) => {
-  try {
-    return fs.readFileSync(path, 'utf-8');
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-writeFileSyncFn('./test.txt', 'Hello Node.js');
-const content = readFileSyncFn('./test.txt');
-console.log(content);
+console.log("Before append");
+appendFileSync("./example.txt","This is the data written throygh sync function:'appendFileSync'");
+console.log("after append");
